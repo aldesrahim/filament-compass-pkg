56338acdeb0c2c28de2ee8c99d39cc981bccbbb1
Al Desrahim <aldesrahim@gmail.com>
2026-04-08 23:06:38 +0700

    remove unused test

--- tests/Unit/ServiceProviderTest.php
+++ tests/Unit/ServiceProviderTest.php
@@ -1,23 +1,8 @@
 <?php
 
 use Aldesrahim\FilamentCompass\FilamentCompassServiceProvider;
-use Spatie\LaravelPackageTools\Package;
 
 it('is registered in the application', function () {
     expect(app()->getProvider(FilamentCompassServiceProvider::class))
         ->toBeInstanceOf(FilamentCompassServiceProvider::class);
-});
-
-it('registers the package under the filament-compass name', function () {
-    $provider = app()->getProvider(FilamentCompassServiceProvider::class);
-
-    expect($provider)->toBeInstanceOf(FilamentCompassServiceProvider::class);
-
-    $reflection = new ReflectionClass($provider);
-    $method = $reflection->getMethod('configurePackage');
-
-    $package = new Package;
-    $method->invoke($provider, $package);
-
-    expect($package->name)->toBe('filament-compass');
 });
